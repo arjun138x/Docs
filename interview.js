42114fc05f1403b66303cf99dd3fedf38b09889a
techmalli333 <techmalli333@gmail.com>
2023-10-28 12:12:07 +0530

    custom flat and count of array

--- interview.js
+++ interview.js
@@ -9,6 +9,17 @@
 // cluster - nodejs is single threaded. by using cluster we can create instances of node
 // pm2 ia a package - nodejs is single threaded. by using cluster we can create instances of node
 
+//  count of array of numbers
+let arr_1 = [2, 3, 4, 5, 4, 3, 2, 6, 7, 7, 7];
+const count = {};
+
+for (let i = 0; i < arr_1.length; i++) {
+  count[arr_1[i]] = count[arr_1[i]] || 0;
+  count[arr_1[i]]++;
+}
+
+console.log({ count });
+
 // sort given array. odd numbers in left and even numbers in right ascending order
 const a = [3, 5, 2, 1, 6, 8];
 for (let i = 0; i < a.length - 1; i++) {
@@ -21,3 +32,42 @@ for (let i = 0; i < a.length - 1; i++) {
 }
 
 console.log(a); // [ 1, 3, 5, 2, 6, 8 ]
+
+/* Flatten following array without using array.flat
+[1,2,[3,4], [5, [6, 7,[8,[9]]]]] into -> [1,2,3,4,5,6,7,8,9] */
+
+// method one
+function customFlatten(arr) {
+  let flattened = [];
+
+  function flattenHelper(arr) {
+    for (let element of arr) {
+      if (Array.isArray(element)) {
+        flattenHelper(element);
+      } else {
+        flattened.push(element);
+      }
+    }
+  }
+
+  flattenHelper(arr);
+  return flattened;
+}
+
+const originalArray = [1, 2, [3, 4], [5, [6, 7, [8, [9]]]]];
+const flattenedArray = customFlatten(originalArray);
+console.log(flattenedArray);
+
+// method two with reduce
+function customFlatArray(arr) {
+  return arr.reduce((result, value) => {
+    if (Array.isArray(value)) {
+      result.push(...customFlatArray(value));
+    } else {
+      result.push(value);
+    }
+    return result;
+  }, []);
+}
+
+console.log(customFlatArray([1, 2, [3, 4], [5, [6, 7, [8, [9]]]]]));
